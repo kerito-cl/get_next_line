5c0fa6139a04f0ca3bc3f3c8e0bdbc4546c3956d
Matias Quero <mquero@c1r5p16.hive.fi>
2024-11-12 19:29:20 +0200

    continue tomorrow

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mquero <mquero@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 13:59:10 by mquero            #+#    #+#             */
-/*   Updated: 2024/11/11 15:11:16 by mquero           ###   ########.fr       */
+/*   Updated: 2024/11/12 19:29:08 by mquero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	int			i;
+	const char	*s;
+	char		*d;
+
+	if (dest == NULL && src == NULL)
+		return (dest);
+	i = 0;
+	d = dest;
+	s = src;
+	while (i < (int)n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
+
 
 size_t  ft_strlcpy(char *dst, const char *src, size_t size)
 {
@@ -58,44 +78,99 @@ char	*ft_strdup(const char *s)
 }
 
 
+char	*resize_buffer(char *temp, int *capacity)
+{
+	char	*newtemp;
+	int	oldcapacity;
 
-char	*get_next_line(int fd)
+	oldcapacity = *capacity;
+	*capacity = *capacity * 2;
+	newtemp = (char *)malloc(sizeof(char) * (*capacity));
+	ft_strlcpy(newtemp, temp, oldcapacity + 1);
+	free(temp);
+	return (newtemp);
+
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+
+char	*ft_strjoin(char *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*dest;
+
+	i = 0;
+	j = 0;
+	dest = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (dest == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	free(s1);
+	return (dest);
+}
+
+
+char	*read_line(int fd, char *temp)
 {
 	static char	buffer[BUFFER_SIZE] = "";
-	char	temp[1024];
-	int	i;
-	char	*line;
 	static int	k = 1;
 	static int	j = 0;
+	int	i;
 
-	line = NULL;
 	i = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0 || k == 0)
-		return (NULL);
-	while (k != 0)
+	if (k == 0 || k == -1)
+		return NULL;
+	while (k > 0)
 	{
-		if (j == 0)
-			k = read(fd, buffer, BUFFER_SIZE);
-		while (j < k && buffer[j] != '\n')
-		{
-			temp[i] = buffer[j];
-			i++;
-			j++;
-		}
-		if (buffer[j] == '\n')
+		k = read(fd, buffer, BUFFER_SIZE);
+		if (k < BUFFER_SIZE)
 			break;
-		j = 0;
+		temp = ft_strjoin(temp, buffer);
 	}
-	if (buffer[j] == '\n' || buffer[j] == '\0')
+	return temp;
+}
+char	*get_next_line(int fd)
+{
+	char	*temp;
+	static int	i = 0;
+	char	*line;
+	int	k;
+
+	k = i;
+	line = NULL;
+	temp = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	temp = read_line(fd, temp);
+	while (temp[i])
 	{
-		temp[i] = buffer[j];
+		if (temp[i] == '\n' || temp[i] == '\0')
+		{
+			line = ft_strdup(temp - k);
+			free(temp);
+			printf("%s\n", line);
+			i++;
+			return (line);
+		}
 		i++;
-		j++;
-		line = (char *)malloc(sizeof(char) * (i + 1));
-		if (line == NULL)
-			return NULL;
-		ft_strlcpy(line, (temp), (size_t)i + 1);
-		return line;
 	}
 	return NULL;
 }
@@ -108,12 +183,13 @@ int	main(void)
 	fd = open("./textfiles/41_with_nl", O_RDONLY);
 	// if (fd == -1)
 	//	printf("Error Number");
-	next = get_next_line(fd);	
-	while (next != NULL)
+	next = get_next_line(fd);
+	//next = get_next_line(fd);
+	/*while (next != NULL)
 	{
 		printf("%s", next);
 		free(next);
 		next = get_next_line(fd);
-	}
+	}*/
 	return (0);
 }
